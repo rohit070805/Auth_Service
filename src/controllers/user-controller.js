ac2a1d82167148a43ccb3e23aa6bda13bc0f7716
Rohit Kumar <rohitdhankhar7347@gmail.com>
2026-01-05 18:53:14 +0530

    added create user funtionality

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,29 @@
+const UserService =require('../services/user-service');
+const {response} = require('express');
+const userService = new UserService();
+
+const create = async (req,res)=>{
+    try {
+        const response = await userService.create({
+            email:req.body.email,
+            password:req.body.password
+        });
+
+         return res.status(200).json({
+            data:response,
+            message:"Successfully created a user",
+            success:true,
+            err:{}
+        });
+    } catch (error) {
+        return res.status(500).json({
+            data:{},
+            message:"Something went wrong",
+            success:false,
+            err:error
+        });
+    }
+}
+module.exports ={
+    create
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const {PORT} = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 
+
 const prepareAndStartserver=()=>{
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use('/api',apiRoutes);
 app.listen(PORT,()=>{
     console.log("server started on port",5000);
 });
